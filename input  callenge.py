097e73053cb5cbed1082513565255b849bd378ad
John Bradley <jontexjesus@gmal.com>
2024-03-06 11:41:58 +0300

    first codes

---
+++ input  callenge.py
@@ -0,0 +1,9 @@
+
+#Week 1 powerlearn project python challenge.
+#assigning input values to a variable.
+name=input("Enter your name;");
+age=input("Enter your age:");
+location=input("Enter your Location:");
+
+#Displaying the output.
+print(f"Hello {name} you are {age} years old and live in {location}");
